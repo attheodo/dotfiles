673c2760d60572507ba84c4c28d1bce992b2e0cf
attheodo <at@atworks.gr>
2016-05-05 15:20:00 +0300

    initial commit

---
+++ brew.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# taps
+brew tap homebrew/services
+
+# packages
+brew install boost
+brew install brew-cask
+brew install ffmpeg
+brew install gdbm
+brew install heroku-toolbelt
+brew install highlight
+brew install lame
+brew install libvo-aacenc
+brew install lua
+brew install mongodb
+brew install namebench
+brew install node
+brew install pcre
+brew install pkg-config
+brew install redis
+brew install tree
+brew install x264
+brew install xvid
+brew install zsh
---
+++ cask.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+
+# taps
+brew tap caskroom/cask
+brew tap caskroom/fonts
+
+# casks
+brew cask install appcleaner
+brew cask install atom
+brew cask install cheatsheet
+brew cask install coconutbattery
+brew cask install cyberduck
+brew cask install dropbox
+brew cask install flux
+brew cask install font-anonymous-pro
+brew cask install font-anonymous-pro-for-powerline
+brew cask install font-arimo
+brew cask install font-coustard
+brew cask install font-dejavu-sans-mono-for-powerline
+brew cask install font-droid-sans-mono-for-powerline
+brew cask install font-fira-mono-for-powerline
+brew cask install font-inconsolata-dz-for-powerline
+brew cask install font-inconsolata-for-powerline
+brew cask install font-josefin-slab
+brew cask install font-lato
+brew cask install font-liberation-mono-for-powerline
+brew cask install font-maven-pro
+brew cask install font-montserrat
+brew cask install font-open-sans
+brew cask install font-oswald
+brew cask install font-source-code-pro-for-powerline
+brew cask install font-source-han-sans
+brew cask install google-chrome
+brew cask install istumbler
+brew cask install iterm2
+brew cask install mou
+brew cask install onyx
+brew cask install postgres
+brew cask install postico
+brew cask install qlmarkdown
+brew cask install skype
+brew cask install slack
+brew cask install sourcetree
+brew cask install spectacle
+brew cask install spotify
+brew cask install sublime-text
+brew cask install transmission
+brew cask install vlc
